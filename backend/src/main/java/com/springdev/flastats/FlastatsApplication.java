24cb8ee80f39f198ba3f4e7f4aa5f735a8c288ca
Diego-Sant <diego_santana@unigranrio.br>
2022-09-07 19:03:44 -0300

    Project created

---
+++ backend/src/main/java/com/springdev/flastats/FlastatsApplication.java
@@ -0,0 +1,13 @@
+package com.springdev.flastats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlastatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlastatsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/springdev/flastats/FlastatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springdev.flastats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlastatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
